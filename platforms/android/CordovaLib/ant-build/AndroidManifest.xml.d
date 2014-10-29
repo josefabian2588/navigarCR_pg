3f87c731788c6e017d9388f424770aceaf41e4bf
Fabián Araya <josefabian2588@gmail.com>
2014-10-29 00:32:29 -0600

    3
    3

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\phonegap\Documents\GitHub\navigarCR_pg\navigarcr\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\phonegap\Documents\GitHub\navigarCR_pg\navigarcr\platforms\android\CordovaLib\AndroidManifest.xml \
