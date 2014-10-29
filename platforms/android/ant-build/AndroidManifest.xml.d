0714553f7a4ec7d62dbc1a154b8082804f8d9a86
Fabián Araya <josefabian2588@gmail.com>
2014-10-29 00:33:06 -0600

    4
    4

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\phonegap\Documents\GitHub\navigarCR_pg\navigarcr\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\phonegap\Documents\GitHub\navigarCR_pg\navigarcr\platforms\android\AndroidManifest.xml \
